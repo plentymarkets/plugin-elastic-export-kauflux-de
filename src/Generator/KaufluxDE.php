3ec1f27deebab09660bb341eb3f4c8d4423ae70d
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2017-02-21 09:30:14 +0100

    plugin.json adjustment

--- src/Generator/KaufluxDE.php
+++ src/Generator/KaufluxDE.php
@@ -7,7 +7,7 @@ use Plenty\Modules\Helper\Services\ArrayHelper;
 use Plenty\Modules\Item\DataLayer\Models\Record;
 use Plenty\Modules\Item\DataLayer\Models\RecordList;
 use Plenty\Modules\DataExchange\Models\FormatSetting;
-use ElasticExportCore\Helper\ElasticExportCoreHelper;
+use ElasticExport\Helper\ElasticExportCoreHelper;
 use Plenty\Modules\Helper\Models\KeyValue;
 use Plenty\Modules\Item\Property\Contracts\PropertySelectionRepositoryContract;
 use Plenty\Modules\Item\Property\Models\PropertySelection;
