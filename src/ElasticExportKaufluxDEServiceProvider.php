06a1c27a86a4c508e7af6f647157417c43b1017f
Alin Migea <alin.migea@plentymarkets.com>
2017-06-09 19:12:52 +0200

    ADD adjustments for check24.de, now the plugin uses just data from ElasticSearch ADD helper classes, removed DataLayer, added logs and updated changelogs ADD the dependency for the ElasticExport plugin

--- src/ElasticExportKaufluxDEServiceProvider.php
+++ src/ElasticExportKaufluxDEServiceProvider.php
@@ -5,13 +5,25 @@ namespace ElasticExportKaufluxDE;
 use Plenty\Modules\DataExchange\Services\ExportPresetContainer;
 use Plenty\Plugin\DataExchangeServiceProvider;
 
+/**
+ * Class ElasticExportKaufluxDEServiceProvider
+ * @package ElasticExportKaufluxDE
+ */
 class ElasticExportKaufluxDEServiceProvider extends DataExchangeServiceProvider
 {
+    /**
+     * Abstract function for registering the service provider.
+     */
     public function register()
     {
 
     }
 
+    /**
+     * Adds the export format to the export container.
+     *
+     * @param ExportPresetContainer $container
+     */
     public function exports(ExportPresetContainer $container)
     {
         $container->add(
@@ -19,6 +31,7 @@ class ElasticExportKaufluxDEServiceProvider extends DataExchangeServiceProvider
             'ElasticExportKaufluxDE\ResultField\KaufluxDE',
             'ElasticExportKaufluxDE\Generator\KaufluxDE',
             '',
+            true,
             true
         );
     }
--- src/ResultField/KaufluxDE.php
+++ src/ResultField/KaufluxDE.php
@@ -2,27 +2,33 @@
 
 namespace ElasticExportKaufluxDE\ResultField;
 
+use Plenty\Modules\Cloud\ElasticSearch\Lib\ElasticSearch;
 use Plenty\Modules\DataExchange\Contracts\ResultFields;
 use Plenty\Modules\DataExchange\Models\FormatSetting;
 use Plenty\Modules\Helper\Services\ArrayHelper;
+use Plenty\Modules\Item\Search\Mutators\BarcodeMutator;
 use Plenty\Modules\Item\Search\Mutators\ImageMutator;
 use Plenty\Modules\Cloud\ElasticSearch\Lib\Source\Mutator\BuiltIn\LanguageMutator;
+use Plenty\Modules\Item\Search\Mutators\KeyMutator;
 use Plenty\Modules\Item\Search\Mutators\SkuMutator;
 use Plenty\Modules\Item\Search\Mutators\DefaultCategoryMutator;
 
 /**
  * Class KaufluxDE
+ * @package ElasticExportKaufluxDE\ResultField
  */
 class KaufluxDE extends ResultFields
 {
     const KAUFLUX_DE = 116.00;
-    /*
+
+    /**
      * @var ArrayHelper
      */
     private $arrayHelper;
 
     /**
-     * Billiger constructor.
+     * KaufluxDE constructor.
+     *
      * @param ArrayHelper $arrayHelper
      */
     public function __construct(ArrayHelper $arrayHelper)
@@ -30,14 +36,21 @@ class KaufluxDE extends ResultFields
         $this->arrayHelper = $arrayHelper;
     }
 
+    /**
+     * Creates the fields set to be retrieved from ElasticSearch.
+     *
+     * @param array $formatSettings
+     * @return array
+     */
     public function generateResultFields(array $formatSettings = []):array
     {
         $settings = $this->arrayHelper->buildMapFromObjectList($formatSettings, 'key', 'value');
 
         $reference = $settings->get('referrerId') ? $settings->get('referrerId') : self::KAUFLUX_DE;
 
-        $itemDescriptionFields = ['texts.urlPath'];
-        $itemDescriptionFields[] = 'texts.keywords';
+        $this->setOrderByList(['item.id', ElasticSearch::SORTING_ORDER_ASC]);
+
+        $itemDescriptionFields = ['texts.urlPath', 'texts.lang', 'texts.keywords'];
 
         switch($settings->get('nameId'))
         {
@@ -79,10 +92,22 @@ class KaufluxDE extends ResultFields
         {
             $imageMutator->addMarket($reference);
         }
+
+        /**
+         * @var KeyMutator $keyMutator
+         */
+        $keyMutator = pluginApp(KeyMutator::class);
+        if($keyMutator instanceof KeyMutator)
+        {
+            $keyMutator->setKeyList($this->getKeyList());
+            $keyMutator->setNestedKeyList($this->getNestedKeyList());
+        }
+
         /**
          * @var LanguageMutator $languageMutator
          */
         $languageMutator = pluginApp(LanguageMutator::class, [[$settings->get('lang')]]);
+
         /**
          * @var SkuMutator $skuMutator
          */
@@ -91,6 +116,7 @@ class KaufluxDE extends ResultFields
         {
             $skuMutator->setMarket($reference);
         }
+
         /**
          * @var DefaultCategoryMutator $defaultCategoryMutator
          */
@@ -98,6 +124,15 @@ class KaufluxDE extends ResultFields
         if($defaultCategoryMutator instanceof DefaultCategoryMutator)
         {
             $defaultCategoryMutator->setPlentyId($settings->get('plentyId'));
+        }
+
+        /**
+         * @var BarcodeMutator $barcodeMutator
+         */
+        $barcodeMutator = pluginApp(BarcodeMutator::class);
+        if($barcodeMutator instanceof BarcodeMutator)
+        {
+            $barcodeMutator->addMarket($reference);
         }
 
         $fields = [
@@ -147,14 +182,21 @@ class KaufluxDE extends ResultFields
                 'barcodes.code',
                 'barcodes.type',
 
-                //attributes
-                'attributes.attributeValueSetId',
+                //properties
+                'properties.property.id',
+                'properties.property.valueType',
+                'properties.selection.name',
+                'properties.selection.lang',
+                'properties.texts.value',
+                'properties.texts.lang',
             ],
 
             [
+                $keyMutator,
                 $languageMutator,
                 $skuMutator,
-                $defaultCategoryMutator
+                $defaultCategoryMutator,
+                $barcodeMutator,
             ],
         ];
 
@@ -169,5 +211,119 @@ class KaufluxDE extends ResultFields
         }
 
         return $fields;
+    }
+
+    /**
+     * Returns the list of keys.
+     *
+     * @return array
+     */
+    private function getKeyList()
+    {
+        $keyList = [
+            //item
+            'item.id',
+            'item.manufacturer.id',
+            'item.free1',
+            'item.free2',
+            'item.free3',
+            'item.storeSpecial',
+
+            //variation
+            'variation.availability.id',
+            'variation.stockLimitation',
+            'variation.vatId',
+            'variation.model',
+            'variation.weightG',
+
+            //unit
+            'unit.content',
+            'unit.id',
+        ];
+
+        return $keyList;
+    }
+
+    /**
+     * Returns the list of nested keys.
+     *
+     * @return mixed
+     */
+    private function getNestedKeyList()
+    {
+        $nestedKeyList['keys'] = [
+            //images
+            'images.item',
+            'images.variation',
+
+            //texts
+            'texts',
+
+            //defaultCategories
+            'defaultCategories',
+
+            //barcodes
+            'barcodes',
+
+            //properties
+            'properties',
+        ];
+
+        $nestedKeyList['nestedKeys'] = [
+            //images
+            'images.item' => [
+                'urlMiddle',
+                'urlPreview',
+                'urlSecondPreview',
+                'url',
+                'path',
+                'position',
+            ],
+
+            'images.variation' => [
+                'urlMiddle',
+                'urlPreview',
+                'urlSecondPreview',
+                'url',
+                'path',
+                'position',
+            ],
+
+            //texts
+            'texts' => [
+                'urlPath',
+                'lang',
+                'texts.keywords',
+                'name1',
+                'name2',
+                'name3',
+                'shortDescription',
+                'description',
+                'technicalData',
+            ],
+
+            //defaultCategories
+            'defaultCategories' => [
+                'id'
+            ],
+
+            //barcodes
+            'barcodes' => [
+                'code',
+                'type',
+            ],
+
+            //proprieties
+            'properties'    => [
+                'property.id',
+                'property.valueType',
+                'selection.name',
+                'selection.lang',
+                'texts.value',
+                'texts.lang',
+            ],
+        ];
+
+        return $nestedKeyList;
     }
 }
